e379aa62c1f9f09a19c272fd0b4ba482b4179adf
Ladislus <walcak.ladislas@gmail.com>
2020-12-29 00:38:32 +0100

    [WIP] Transfer

---
+++ Assets/Scripts/moveBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBackground : MonoBehaviour {
+    
+    public Transform respawn;
+
+    public float speed;
+    
+    private float height;
+    private float bottom;
+
+    void Start() {
+        
+        GetComponent<RigidBody2D>().velocity = new Vector2(0, -this.speed);
+
+        this.bottom = Camera.main.ViewToWorldPoint(new Vector(0, 0, 0)).y;
+        this.height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}

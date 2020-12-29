b272f64117b593923c8fdbc510f55beddb5cf0b5
Ladislus <walcak.ladislas@gmail.com>
2020-12-29 18:12:23 +0100

    [ADD] Added IGun and Gun

---
+++ Assets/Scripts/Guns/Gun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Gun : IGun {
+
+    public float cooldown;
+    public float min_damages;
+    public float max_damages;
+
+    private float current_cooldown = cooldown;
+
+    void cooldown() {
+        this.current_cooldown = this.current_cooldown - Time.deltaTime;
+        if (this.current_cooldown <= 0) {
+            shoot();
+            this.current_cooldown = this.cooldown;
+        }
+    }
+}

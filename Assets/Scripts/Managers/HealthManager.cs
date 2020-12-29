d3c52b05083a702849f93852cbe6377376e57d60
Ladislus <walcak.ladislas@gmail.com>
2020-12-29 14:16:29 +0100

    [ADD] Added singleton templates for all 3 managers

---
+++ Assets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    private static HealthManager _instance;
+
+    void Awake() {
+        if (_instance == null) { 
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject); 
+        }
+        else Destroy(this); 
+    }
+}
---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    private static ScoreManager _instance;
+
+    void Awake() {
+        if (_instance == null) { 
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject); 
+        }
+        else Destroy(this); 
+    }
+}
